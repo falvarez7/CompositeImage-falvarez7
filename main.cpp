c3ea013089562f56a6542b4aa2bff13924849300
falvarez7 <falvarez7@mail.csuchico.edu>
2017-11-13 14:39:08 -0800

    Progress made, initial string vector generating loop is perfect

--- main.cpp
+++ main.cpp
@@ -5,21 +5,24 @@
 #include"bitmap.h"
 
 using namespace std;
+
 //function prototypes
-bool sameSize(Bitmap, Bitmap);
+bool sameSize(string, string);
 int colSize(Bitmap);
 int rowSize(Bitmap);
 bool validFile(string);
+
 //variable declaration
+const int MAX_FILES = 5;
 vector<string> fileVector;
-Bitmap picture1, picture2;
 bool tester;
-bool rightSize = false;
 string filename,filename1;
-int height, width,size;
+int size;
 int main()
 {
     cout<<"Enter the name of 2 files."<<endl;
+    do
+    {
     cin>>filename1;
     tester = validFile(filename1);
     if(tester ==true)
@@ -32,16 +35,24 @@ int main()
         {
         cout<<"invalid file, try again"<<endl;
         };
-    do
+    }
+    while(tester!=true);
+    while(fileVector.size()<MAX_FILES || filename != "Done")
+
         {
         bool tester1,tester2;
+        if(fileVector.size()<MAX_FILES)
+        {
         cin>>filename;
-        tester1 = validFile(filename);
-        if(tester1 == true)
+        if(filename=="Done")
             {
-            picture1.open(filename1);
-            picture2.open(filename);
-            tester2 = sameSize(picture1,picture2);
+            break;
+            }
+        else if(filename !="Done" && fileVector.size()<=MAX_FILES-1 )
+            {tester1 = validFile(filename);
+                if(tester1 == true)
+                    {
+                      tester2 = sameSize(filename,filename1);
             if(tester2==true)
                 {
                 fileVector.push_back(filename);
@@ -52,15 +63,22 @@ int main()
                 cout<<"These pictures are not the same size"<<endl;
                 }
             }   
-        else
+         else
             {
             cout<<"invalid File"<<endl;
             }
-        size = fileVector.size();
-        cout<<"size is: "<<size<<endl;
+
+         }
+         }
+        else
+            {
+        break;
         }
-    while(fileVector.size()<=9 || filename != "Done");
-    cout<<"You have finished this loop successfully"<<endl;
+
+         size = fileVector.size();
+        cout<<"size is: "<<size<<endl;
+            }
+        cout<<"You have finished this loop successfully"<<endl;
 return 0; 
 }
 
@@ -74,12 +92,15 @@ bool validFile(string file)
     return valid;
 }
 
-bool sameSize(Bitmap file1,Bitmap file2)
+bool sameSize(string file1,string file2)
 {
-        int width1 = colSize(file1);
-        int width2 = colSize(file2);
-        int height1 = rowSize(file1);
-        int height2 = rowSize(file2);
+        Bitmap bit1, bit2;
+        bit1.open(file1);
+        bit2.open(file2);
+        int width1 = colSize(bit1);
+        int width2 = colSize(bit2);
+        int height1 = rowSize(bit1);
+        int height2 = rowSize(bit2);
         if (width1==width2 && height1 == height2)
            {
            return true;
@@ -102,30 +123,11 @@ int colSize(Bitmap bitmap)
 }
 
 
-//1.) A do while loop which has a boolean condition so it repeats until the correct amount of files or correct format have been entered, or the input is done. This loop will also check the size of the first image as reference for the following images. These images will be checked by a function called samesize.
-
-
-
-
-
-
-
-
-
-
-    //a.) inside this loop, have an if statement which, if the format and size of the entered file is correct, adds 1 to the count of correctly entered files.
-   
-   //b.) inside this loop, have an else statement which prints an error message for any file format other than bmp
-   
-
-//2.) Have another if statment outside of this loop which will use the amount from 1.)a.) to check if more than 2 files have been correctly entered and then either return true or false. 
-
-//3.)Have another if and else statment to either end the program if enough images are entered, or continue with the program. 
-
 //4.)Inside the continue loop, have the first image converted into a pixel matrix and set the variable composite matrix  equal to the first pixel matrix. After this initial conversion has occured, the rest of the bitmaps will be converted and averaged into the composite matrix in a function called Compositor within a for loop. This loop will also print out how many bitmaps have been converted. 
 
 //Compositor recieves the composite pixel matrix and one of the iterated Bitmaps. First it converts the iterated bitmap into a pixel matrix. It then averages the two pixel matrices and sets compMatrix equal to the new matrix(Why it's pass-by-reference).
-//void compositor(vector< vector<Pixel> > &, Bitmap)
+vector <vector <Pixel> > compositor(vector <string>)
+{
 
 //Averages are not the sum of averages.
 //5.)Once all the matrices have been ran through compositor, compMatrix will be converted back to a Bitmap. 
